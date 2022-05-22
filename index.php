91577de6c0c2e8a9c0ab5da12b7d09eabdebcef5
Dosztosz <dosztosz@int.pl>
2022-05-22 11:14:16 +0200

    edit 1.0 11:14 22.05.2022

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library managment system</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <script src="https://kit.fontawesome.com/8d0eb52cc9.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="sidebar" style="width:60px;">
+  <a href="#" class="bar-item"><i class="fa fa-house-user"></i></a> 
+  <a href="#" class="bar-item"><i class="fa fa-search"></i></a> 
+  <a href="#" class="bar-item"><i class="fa fa-envelope"></i></a> 
+  <a href="#" class="bar-item"><i class="fa fa-globe"></i></a>
+  <a href="#" class="bar-item"><i class="fa fa-trash"></i></a> 
+</div>
+
+<div class="content">
+    <h2>Sidebar Icons</h2>
+    <p>In this example we have used icons in the sidebar.</p>
+</div>
+
+</div>
+</body>
+</html>
