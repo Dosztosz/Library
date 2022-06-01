4032443a27b6dc35fa90efcb50be2f78cdcd3660
Dosztosz <dosztosz@int.pl>
2022-06-01 13:08:59 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,8 @@
 <div class="sidenav">
     <a href="home.php" class="bar-item"><i class="fa fa-house-user"></i><span class="mobile">Main Page</span></a>
     <a href="rent.php" class="bar-item"><span class="mobile">Rent a costume</a>
-    <a href="rented.php" class="bar-item"><span class="mobile">Rented Books</a>
-    <a href="overdue.php" class="bar-item"><span class="mobile">Overdue Books</a>
+    <a href="rented.php" class="bar-item"><span class="mobile">Rented Costumes</a>
+    <a href="overdue.php" class="bar-item"><span class="mobile">Overdue Rentals</a>
     <div class="dropdown">
         <a class="dropbtn">Costumes 
         <i class="fa fa-caret-down"></i>
