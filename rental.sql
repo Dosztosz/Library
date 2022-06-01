4824ebb48a54f109df176608bcb68b5477b14157
Dosztosz <dosztosz@int.pl>
2022-06-01 12:18:00 +0200

    sql

--- rental.sql
+++ rental.sql
@@ -3,13 +3,19 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mariadb106.server723887.nazwa.pl:3306
--- Czas generowania: 30 Maj 2022, 08:45
+-- Czas generowania: 01 Cze 2022, 12:15
 -- Wersja serwera: 10.6.7-MariaDB-log
 -- Wersja PHP: 7.2.24-0ubuntu0.18.04.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Baza danych: `server723887_rental`
@@ -18,11 +24,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktura tabeli dla tabeli `categories`
+--
+
+CREATE TABLE `categories` (
+  `id_category` int(11) NOT NULL,
+  `name_category` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin2;
+
+-- --------------------------------------------------------
+
+--
 -- Struktura tabeli dla tabeli `costumes`
 --
 
 CREATE TABLE `costumes` (
-  `id` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
   `number` varchar(100) NOT NULL,
   `name_costume` varchar(100) NOT NULL,
   `size` varchar(100) NOT NULL,
@@ -35,11 +52,11 @@ CREATE TABLE `costumes` (
 -- Zrzut danych tabeli `costumes`
 --
 
-INSERT INTO `costumes` (`id`, `number`, `name_costume`, `size`, `price`, `quantity`, `category_id`) VALUES
-(1, '1', 'Indianin', '104', '30', '1', '1'),
-(2, '1', 'Indianin', '116', '30', '1', '1'),
-(3, '1', 'Indianin', '128', '30', '1', '1'),
-(4, '1', 'Indianin', 'Duży', '30', '1', '1');
+INSERT INTO `costumes` (`id_product`, `number`, `name_costume`, `size`, `price`, `quantity`, `category_id`) VALUES
+(1, '1', 'Indianin', '104', '30', '2', '1'),
+(2, '60', 'Rycerz', '116', '30', '0', '1'),
+(3, '259', 'Marsjanin', '128', '30', '17', '1'),
+(4, '360', 'Star Wars', 'Duży', '30', '14', '1');
 
 -- --------------------------------------------------------
 
@@ -48,7 +65,7 @@ INSERT INTO `costumes` (`id`, `number`, `name_costume`, `size`, `price`, `quanti
 --
 
 CREATE TABLE `rentals` (
-  `id` int(11) NOT NULL,
+  `id_rent` int(11) NOT NULL,
   `name` varchar(40) NOT NULL,
   `date_rent` date NOT NULL,
   `date_return` date NOT NULL,
@@ -63,11 +80,11 @@ CREATE TABLE `rentals` (
 -- Zrzut danych tabeli `rentals`
 --
 
-INSERT INTO `rentals` (`id`, `name`, `date_rent`, `date_return`, `costume_id`, `price`, `phone`, `address`, `status`) VALUES
-(1, 'Tomasz Dębosz', '2022-05-26', '2022-05-28', '1', '30', '+48506409529', 'Józefa Romana 25', 'rented'),
-(2, 'Borkosz Mariusz', '2022-05-26', '2022-05-29', '2', '30', '+48506432329', 'Nowohucka 35 32-700 Bochnia', 'rented'),
-(3, 'dsadsa', '2022-05-29', '2022-05-30', '3', 'sadasdas', 'dsadsa', 'address', 'overdue'),
-(4, 'JarosĹaw Krawczyk', '2022-05-30', '2022-06-01', '1', '30 zĹ', '+48 506 409 529', 'address', 'done'),
+INSERT INTO `rentals` (`id_rent`, `name`, `date_rent`, `date_return`, `costume_id`, `price`, `phone`, `address`, `status`) VALUES
+(1, 'Tomasz Dębosz', '2022-05-26', '2022-05-28', '1', '30', '+48506409529', 'Józefa Romana 25', 'overdue'),
+(2, 'Borkosz Mariusz', '2022-05-26', '2022-05-29', '2', '30', '+48506432329', 'Nowohucka 35 32-700 Bochnia', 'overdue'),
+(3, 'dsadsa', '2022-05-29', '2022-05-29', '3', 'sadasdas', 'dsadsa', 'address', 'overdue'),
+(4, 'JarosĹaw Krawczyk', '2022-05-30', '2022-06-01', '1', '30 zĹ', '+48 506 409 529', 'address', 'rented'),
 (5, 'Kuba Samek', '2022-05-31', '2022-06-02', '1', '30 zł', '+48 506 409 529', 'Jóżefa Romana 25', 'rented'),
 (6, 'Tomasz Dębosz', '2022-05-29', '2022-05-31', '3', '30 zł', '+48 506 409 529', 'Józefa Romana 25', 'rented');
 
@@ -78,7 +95,7 @@ INSERT INTO `rentals` (`id`, `name`, `date_rent`, `date_return`, `costume_id`, `
 --
 
 CREATE TABLE `sizes` (
-  `id` int(11) NOT NULL,
+  `id_sizes` int(11) NOT NULL,
   `size` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin2;
 
@@ -86,7 +103,7 @@ CREATE TABLE `sizes` (
 -- Zrzut danych tabeli `sizes`
 --
 
-INSERT INTO `sizes` (`id`, `size`) VALUES
+INSERT INTO `sizes` (`id_sizes`, `size`) VALUES
 (1, '104'),
 (2, '116'),
 (3, '128'),
@@ -101,7 +118,7 @@ INSERT INTO `sizes` (`id`, `size`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id_users` int(11) NOT NULL,
   `user` varchar(100) NOT NULL,
   `pass` varchar(100) NOT NULL,
   `power` varchar(100) NOT NULL
@@ -111,62 +128,78 @@ CREATE TABLE `users` (
 -- Zrzut danych tabeli `users`
 --
 
-INSERT INTO `users` (`id`, `user`, `pass`, `power`) VALUES
-(1, 'dosztosz', 'dosztosz', '3');
+INSERT INTO `users` (`id_users`, `user`, `pass`, `power`) VALUES
+(1, 'admin', 'admin', '3');
 
 --
 -- Indeksy dla zrzutów tabel
 --
 
 --
+-- Indeksy dla tabeli `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id_category`);
+
+--
 -- Indeksy dla tabeli `costumes`
 --
 ALTER TABLE `costumes`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_product`);
 
 --
 -- Indeksy dla tabeli `rentals`
 --
 ALTER TABLE `rentals`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_rent`);
 
 --
 -- Indeksy dla tabeli `sizes`
 --
 ALTER TABLE `sizes`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_sizes`);
 
 --
 -- Indeksy dla tabeli `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_users`);
 
 --
 -- AUTO_INCREMENT dla zrzuconych tabel
 --
 
 --
+-- AUTO_INCREMENT dla tabeli `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT dla tabeli `costumes`
 --
 ALTER TABLE `costumes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT dla tabeli `rentals`
 --
 ALTER TABLE `rentals`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_rent` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT dla tabeli `sizes`
 --
 ALTER TABLE `sizes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_sizes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_users` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
