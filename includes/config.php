9a74318b9efa8a0d50f2165d2189ad6684db20bf
Dosztosz <dosztosz@int.pl>
2022-06-01 12:16:07 +0200

    connect edit

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
-$servername = "mariadb106.server723887.nazwa.pl";
-$username = "server723887_rental";
-$password = "Lanarith3945";
-$db_name = "server723887_rental";
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$db_name = "rental";
 ?>
