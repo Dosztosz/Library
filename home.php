36e831d329a557f96f02d3dfb1d1eaab4829c83c
Dosztosz <dosztosz@int.pl>
2022-06-10 12:13:06 +0200

    added custom graph

--- home.php
+++ home.php
@@ -25,6 +25,11 @@
 
 <head>
     <?php include ('includes/head.php') ?>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+
 </head>
 
 <body>
@@ -71,30 +76,31 @@
                 </div>
             </div>
             <div class="dash-column">
-                <h2 class="rental-box-h2">Upcoming Rentals</h2>
-                <div class="rental-box-dash">
-                    <div class="rent-box-dash">
-                        <div class="rent-box-info">
-                            <p class="info-stroj">Indianin 104</p>
-                            <p class="info-imie">Tomasz Pekasz</p>
-                            <p>Zapłacone</p>
-                            <p>09.06.2022</p>
-                            <button class="button">Confirm</button>
-                        </div>
-                    </div>
-                    <div class="rent-box-dash">
-                        <div class="rent-box-info">
-                        <p>Indianin 104</p>
-                        <p>Tomasz Pekasz</p>
-                        <p>Zapłacone</p>
-                        <p>09.05.2022</p>
-                        <button class="button">Confirm</button>
-                        </div>
-                    </div>
-                </div>
+                <div id="myfirstchart" style="height: 250px;"></div>
             </div>
         </div>
-        
     </div>
+    <script>
+        new Morris.Line({
+  // ID of the element in which to draw the chart.
+  element: 'myfirstchart',
+  // Chart data records -- each entry in this array corresponds to a point on
+  // the chart.
+  data: [
+    { year: '2008', value: 30 },
+    { year: '2009', value: 20 },
+    { year: '2010', value: 4 },
+    { year: '2011', value: 2 },
+    { year: '2012', value: 20 }
+  ],
+  // The name of the data record attribute that contains x-values.
+  xkey: 'year',
+  // A list of names of data record attributes that contain y-values.
+  ykeys: ['value'],
+  // Labels for the ykeys -- will be displayed when you hover over the
+  // chart.
+  labels: ['Value']
+});
+</script>
 </body>
 </html>
