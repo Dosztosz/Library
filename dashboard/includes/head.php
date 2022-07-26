ebae92ff968d9f0fb12e6545699acb5ca8adfe55
Dosztosz <dosztosz@int.pl>
2022-07-26 15:07:33 +0200

    update

--- dashboard/includes/head.php
+++ dashboard/includes/head.php
@@ -1,6 +1,5 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php echo $site_title; ?></title>
+<title>Wypożyczalnia <?php echo $site_title; ?></title>
 <link rel="stylesheet" href="styles/style.css?ve">
-<script src="https://kit.fontawesome.com/8d0eb52cc9.js" crossorigin="anonymous"></script>
---
+++ testing_cat.php
@@ -0,0 +1,13 @@
+<?php
+
+$array = ['adamowicz', 'kebabowicz', 'martynowicz', 'tomaszowicz'];
+$php = 4;
+
+for ($i = 0; $i < $php; $i++)
+{
+    echo $array[$i];
+}
+
+
+
+?>
